441eb0aff8e86f7530373981765a2f9e78c6d5f0
Muhammad umar <153982980+Umarkeerio@users.noreply.github.com>
2024-10-22 04:24:11 -0700

    Create EX1.cpp

---
+++ EX1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num1 = 3;
+    int num2 = 2;
+
+    cout << "addition of " << num1 << " and " << num2 << " is " << num1 + num2 << endl;
+
+    return 0;
+}
